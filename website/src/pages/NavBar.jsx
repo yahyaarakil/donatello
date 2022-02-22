a3f657d6c0b675d9416a6a54900421621e965dad
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-02-22 12:18:18 +0300

    updated

--- website/src/pages/NavBar.jsx
+++ website/src/pages/NavBar.jsx
@@ -8,8 +8,7 @@ export default function NavBar() {
                 <img src={donatelloLogo} className="nav-logo" alt="donatello-logo" />
                 <li><a href="contact">Contact</a></li>
                 <li><a href="about">About</a></li>
-                <li><a href="settings">Settings</a></li>
-                <li><a href="login">Login</a></li>
+                <li><a href="login">Login</a></li>    
             </ul>
         </nav>
 
