6479bbbeec53305981ef61c930b1d424e1c5d7ab
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-02-22 12:16:17 +0300

    Create HomePage.jsx

---
+++ website/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import '../styles/HomePage.css';
+import LoginPage from './LoginPage';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default HomePage;
