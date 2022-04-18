ff3612687ddfcd7ff67ec194a73aba7aa1f653a7
GreatSum <oguzkaganaltas@gmail.com>
2022-04-18 10:56:20 +0300

    Profile page divided into 2

--- website/src/pages/ProfilePage.jsx
+++ website/src/pages/ProfilePage.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import axios from 'axios';
 import LoginNavBar from './LoginNavBar';
+import Footer from './Footer';
 
 import { useRef, useState, useEffect } from 'react';
 import "../styles/ProfilePage.css"
 
 export const ProfilePage = () => {
-    
-
 
     return (
-      
+
         <>
-            <LoginNavBar />
-           
-         
-            
+            <LoginNavBar/>
+            <div class="wrapper">
+                <div class="main">
+                    <LeftBody />
+                </div>
+                <div class="sidebar">
+                    <RightBody />
+                </div>
+            </div>
+            <Footer/>
         </>
     )
 }
+
+function RightBody() {
+
+    return (
+        <>
+            <div className="user-details">
+                <p>Email Address</p>
+            </div>
+        </>
+    )
+
+
+}
+
+function LeftBody() {
+
+    return (
+        <>
+            <div>
+                <p>
+                    hello
+                </p>
+            </div>
+        </>
+    )
+}
+
+
 
 export default ProfilePage
