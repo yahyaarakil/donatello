04d696a1702c7db248e1ed8b3709d399dbcbb0e5
GreatSum <oguzkaganaltas@gmail.com>
2022-04-18 11:31:32 +0300

    basic details added

--- website/src/pages/ProfilePage.jsx
+++ website/src/pages/ProfilePage.jsx
@@ -11,7 +11,7 @@ export const ProfilePage = () => {
     return (
 
         <>
-            <LoginNavBar/>
+            <LoginNavBar />
             <div class="wrapper">
                 <div class="main">
                     <LeftBody />
@@ -20,7 +20,7 @@ export const ProfilePage = () => {
                     <RightBody />
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
@@ -30,7 +30,37 @@ function RightBody() {
     return (
         <>
             <div className="user-details">
-                <p>Email Address</p>
+                <div class="card-body" id="yui_3_17_2_1_1650266273621_24">
+                    <h3 class="lead">User details</h3>
+                    <ul id="yui_3_17_2_1_1650266273621_23">
+                        <li class="editprofile"><span><a href="http://localhost:3000/profile">Save Changes</a></span></li>
+
+                        <li class="contentnode">
+                            <dl>
+                                <dt>Name</dt>
+                                <dd>Master</dd>
+
+                                <dt>Surname</dt>
+                                <dd>Splinter</dd>
+
+                                <dt>Email address</dt>
+                                <dd><a href="mailto:oguz.altas@metu.edu.tr">oguz.altas@metu.edu.tr</a></dd>
+
+
+                            </dl>
+                        </li>
+
+                        <li className='vessels-operating'>
+                            <label for="cars">Choose a vessel to remove:</label>
+                            <select name="cars" id="cars">
+                                <option value="Girne Donatello">Girne Donatello</option>
+                                <option value="Karpaz Donatello">Karpaz Donatello</option>
+                                <option value="Lefke Donatello">Lefke Donatello</option>
+                                <option value="Magusa Donatello">Magusa Donatello</option>
+                            </select>
+                        </li>
+
+                    </ul></div>
             </div>
         </>
     )
