1421bc29d70127a09f6a267d3dc81570cd7ce63a
Yahya Arakil <yahyaarakil@gmail.com>
2022-04-29 19:47:46 +0300

    Connected to DB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.listen(8000, () => {
     console.log('Starting Donatello Server');
     console.log('Establishing connection to Database');
+    mongoose.connect(process.env.MONGODB_URL).then(() => {
+        console.log('Connected to Database successfully');
+        serve_donatello();
+    }).catch(err => {
+        console.log('Unable to connect to Database, EXITING');
+    });
 });
