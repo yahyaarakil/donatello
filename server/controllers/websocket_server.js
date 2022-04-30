997d4a91382baacc70bde25cb9c1addebe72fb89
Yahya Arakil <yahyaarakil@gmail.com>
2022-04-30 20:36:28 +0300

    Added ws heartbeat and connection dropping

--- server/controllers/websocket_server.js
+++ server/controllers/websocket_server.js
@@ -5,17 +5,33 @@ module.exports.serveDonatello = (WSS_PORT) => {
         try {
             const wss = new WS.WebSocketServer({ port: WSS_PORT });
             wss.on('connection', (ws) => {
+                console.log('Connection attempt to Websocket Server');
                 // authenticate drone
 
+                
                 // on message
                 ws.on('message', (data) => {
                     console.log('received: %s', data);
                     // ws.send('Received');
                 });
+
+
+                // heartbeat
+                ws.isAlive = true;
+                ws.on('pong', () => ws.isAlive = true);
+            
+                const interval = setInterval(() => {
+                    if (ws.isAlive === false) ws.terminate();
+                    ws.isAlive = false;
+                    ws.ping();
+                }, 3000);
+                
+                ws.on('close', () => {
+                    console.log('Connection closed by client');
+                    clearInterval(interval);
+                });
             });
-            wss.on('close', (ws) => {
-                console.log('Connection closing by client');
-            });
+
             resolve(wss);
         }
         catch(err) {
