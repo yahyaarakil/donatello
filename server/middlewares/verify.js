3f96379c283adbf460d17b1b7acaf040c046f5b9
Yahya Arakil <yahyaarakil@gmail.com>
2022-05-30 19:18:12 +0300

    Fixed drone disconnection

--- server/middlewares/verify.js
+++ server/middlewares/verify.js
@@ -4,8 +4,6 @@ const verifyRequest = (constraints) => {
         for (let index = 0; index < Object.entries(constraints).length; index++) {
             const attribute = Object.entries(constraints)[index];
             if (!req.body[attribute[0]] && attribute[1].required) {
-                console.log(req);
-                console.log(req.body);
                 return res.status(400).json({ message: 'Malformed Request' });
             }
         }
