e35ad57182ed3dd136d54654e6ff440f460d3689
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-06-21 18:58:18 +0300

    Update EditMission.jsx

--- website/src/pages/EditMission.jsx
+++ website/src/pages/EditMission.jsx
@@ -1,141 +1,101 @@
 import React from 'react';
 import axios from 'axios';
 import LoginNavBar from './LoginNavBar';
-import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet';
-import { useRef, useState, useEffect } from 'react';
-import { EditControl } from "react-leaflet-draw"
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
 
 import "leaflet-draw/dist/leaflet.draw.css"
-
 import "../styles/CreateMission.css"
 
-function Map({ setMyVar }) {
-    const [editableFG, setEditableFG] = useState(null);
-    // const [coordinate, setCoordinate] = useState([35.247051, 33.024617]);
+export const EditMission = () => {
+    const DEVICES = [{ id: 1, name: "donatello-1" }, { id: 2, name: "donatello-2" }, { id: 3, name: "donatello-3" }];
+    const [device, setDevice] = useState("");
+    const [visible = false, setVisible] = useState("");
 
-    function getCorners(layer) {
-        const corners = layer.getBounds();
 
-        const northwest = corners.getNorthWest();
-        const northeast = corners.getNorthEast();
-        const southeast = corners.getSouthEast();
-        const southwest = corners.getSouthWest();
-
-        return [northwest, northeast, southeast, southwest];
+    function onShowButtonClick() {
+        console.log("adasdsad")
+        setVisible(true);
     }
 
+    function onMANUALButtonClick() {
+        console.log("manual")
+    }
 
-    const onCreated = e => {
+    function onRTLButtonClick() {
+        console.log("rtl")
+    }
 
-        var type = e.layerType,
-            layer = e.layer;
-        if (type === 'rectangle') {
-            layer.on('mouseover', function () {
-                var temp = getCorners(layer);
-                console.log(temp)
-                temp.top = temp[0].lat;
-                temp.bottom = temp[2].lat;
-                temp.left = temp[0].lng;
-                temp.right = temp[2].lng;
-                console.log(temp)
-                setMyVar(temp);
-
-            });
-        }
-
-
-    };
-
-    const onFeatureGroupReady = reactFGref => {
-        // store the ref for future access to content
-        setEditableFG(reactFGref);
-    };
-
-    return (
-        <MapContainer center={[35.247051, 33.024617]} zoom={12}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
-            />
-
-            <FeatureGroup
-                ref={featureGroupRef => {
-                    onFeatureGroupReady(featureGroupRef);
-                }}>
-                <EditControl position="topright"
-                    draw={{
-                        marker: false,
-                        circle: false,
-                        circlemarker: false,
-                        polyline: false,
-                    }}
-                    onCreated={onCreated} />
-            </FeatureGroup>
+    function ShowMission(props) {
+        let device = DEVICES.find(({ id }) => id === props.selectedDevice)
+        console.log(props)
+        if (props.visible) {
+            return (
+                <div className="card mt-5">
+                    <div className="card-body">
+                        <div className='row'>
+                            <div className='col-6 d-flex flex-column'>
+                                <label htmlFor="missionName">Mission Name:</label>
+                                <input id="missionName" type="text" disabled={true} value={device.name}></input>
+                                <label htmlFor="Name">Name:</label>
+                                <input id="Name" type="text" disabled={true} value={device.name}></input>
+                            </div>
+                            <div className='col-6 d-flex flex-column'>
+                            <button onClick={onRTLButtonClick}>RTL</button>
+                            <button onClick={onMANUALButtonClick}>MANUAL</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             )
-        </MapContainer>
-    );
-}
+        }
+        return (<></>);
+    }
 
-function Body({ myVar }) {
     return (
-        <div className="body-div">
-            <ul className="mission">
-                <div>
-                    <div>
-                        <select class="selectpicker" data-style="btn-info" name="selectpicker">
-                            <optgroup label="Select Device">
-                                <option name="table1" value="1">Device 1</option>
-                                <option name="table2" value="2">Device 2</option>
-                            </optgroup>
-                        </select>
-                    </div>
-                    <div>
-                        <p>top: {myVar.top}</p>
-                        <p>bottom: {myVar.bottom}</p>
-                        <p>left: {myVar.left}</p>
-                        <p>right: {myVar.right}</p>
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            id='missionName'
-                            autoComplete="off"
-                            required
-                            placeholder="Mission Name"
-                        />
-                    </div>
-                    
-                    <div>
-                        <button>Clean Area</button>
-                    </div>
-
-                </div>
-            </ul>
-        </div>
-    )
-}
-
-export const CreateMission = () => {
-    const [myVar, setMyVar] = useState({
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-    });
-    
-    return (
-        <div>
+        <>
             <LoginNavBar />
-            <div className="wrapper">
-                <div className="map">
-                    <Map setMyVar={setMyVar} />
-                </div>
-                <div className="sidebody">
-                    <Body myVar={myVar} />
+            <div className='container'>
+                <div className='row'>
+
+
+                    <div className='col-6'>
+                        <div className="map" style={{ width: "100%" }}>
+                            <MapContainer center={[35.247051, 33.024617]} zoom={12}>
+                                <TileLayer
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    attribution='&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
+                                />
+                            </MapContainer>
+                        </div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='card mt-5'>
+                            <div className='card-body'>
+                                <h4 className="card-title">Edit the mission</h4>
+                                <p className="card-text">
+                                    Select an operation:
+                                </p>
+                                <div className='row'>
+                                    <div className='col-6 page-hero d-flex align-items-center justify-content-center'>
+                                        <select className="form-select" onClick={e => setVisible(!e.target.value)} onChange={e => setDevice(e.target.value)}>
+                                            <option key={-1}>select</option>
+                                            {DEVICES.map((missionOptions) => <option key={missionOptions.id} value={missionOptions.id}>{missionOptions.name}</option>)}
+                                        </select>
+                                    </div>
+                                    <div className='col-6 page-hero d-flex align-items-center justify-content-center'>
+                                        <button onClick={onShowButtonClick}>Show</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <ShowMission visible={visible} selectedDevice={Number(device)} />
+                    </div>
+
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
-export default CreateMission;
+export default EditMission;
